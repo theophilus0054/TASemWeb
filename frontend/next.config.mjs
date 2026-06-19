b61519f8787325ab070d30f914fa08b0ece0daaf
Theophilus Samuel <theosamuel2005@gmail.com>
2026-06-19 07:21:12 +0700

    update: update things

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://localhost:5000'}/api/:path*`,
       },
     ];
   },
